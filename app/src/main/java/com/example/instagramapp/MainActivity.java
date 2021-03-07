de93ebd8247663e53f695c41d68c30e873c2dacc
Errol Roberts <errol.me@gmail.com>
2021-03-07 08:21:36 -0500

    post querying complete

--- app/src/main/java/com/example/instagramapp/MainActivity.java
+++ app/src/main/java/com/example/instagramapp/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.instagramapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String tag = "MainActivity";
+    private EditText etDescription;
+    private Button selectBtn;
+    private Button postBtn;
+    private ImageView postImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.description);
+        selectBtn = findViewById(R.id.uploadBtn);
+        postBtn = findViewById(R.id.postBtn);
+        postImage = findViewById(R.id.postImage);
+
+        queryPosts();
+    }
+
+    private void queryPosts(){
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.key_user);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(tag, "not null!", e);
+                    return;
+                }
+                for(Post post : posts){
+                    Log.i(tag, "post: " + post.getDescription() + ", by " + post.getUser());
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/instagramapp/Post.java
@@ -0,0 +1,37 @@
+package com.example.instagramapp;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class Post  extends ParseObject{
+     public static final String key_description = "description";
+     public static final String key_image = "image";
+     public static final String key_user = "user";
+
+    public String getDescription() {
+        return getString(key_description);
+    }
+
+    public void setDescription(String description){
+        put(key_description, description);
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(key_image);
+    }
+
+    public void setImage(ParseFile parseFile){
+        put(key_image, parseFile);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser(key_user);
+    }
+
+    public void setUser(ParseUser user){
+        put(key_user, user);
+    }
+}
